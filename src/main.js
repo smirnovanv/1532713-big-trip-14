a49f46a61aee5d19bd6f7b6aacc175f1b97abad4
smirnovanv <smirnovanv89@gmail.com>
2021-04-11 11:35:58 +0300

    Выход из карточки по Esc, заглушка для пустого списка

--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ import {render, RenderPosition} from './utils.js';
 import TripEventsListView from './view/events-list.js';
 import {generateEvent} from './mock/event.js';
 import {generatePossibleOffers} from './mock/offer.js';
+import EmptyList from './view/empty-list.js';
 
-const POINTS_COUNT = 5;
+const POINTS_COUNT = 3;
 
 const points = new Array(POINTS_COUNT).fill().map(generateEvent);
 const possibleOffers = generatePossibleOffers();
@@ -23,7 +24,6 @@ const tripEvents = document.querySelector('.trip-events');
 const renderPoint = (pointsListElement, point, typeOffers) => {
   const pointComponent = new RoutePointView(point);
   const pointEditComponent = new EditFormView(point, typeOffers);
-  //points[0], possibleOffers
   const replacePointByForm = () => {
     pointsListElement.replaceChild(pointEditComponent.getElement(), pointComponent.getElement());
   };
@@ -32,31 +32,47 @@ const renderPoint = (pointsListElement, point, typeOffers) => {
     pointsListElement.replaceChild(pointComponent.getElement(), pointEditComponent.getElement());
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormByPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   pointComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
     replacePointByForm();
+    document.addEventListener('keydown', onEscKeyDown);
   });
 
   pointEditComponent.getElement().querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
     replaceFormByPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   pointEditComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
     replaceFormByPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   render(pointsListElement, pointComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
-render(tripMain, new RouteInfoAndCostView(points).getElement(), RenderPosition.AFTERBEGIN);
+const renderBoard = (boardContainer, events) => {
+  if (events.length < 1) {
+    render(boardContainer, new EmptyList().getElement(), RenderPosition.BEFOREEND);
+  } else {
+    render(tripMain, new RouteInfoAndCostView(events).getElement(), RenderPosition.AFTERBEGIN);
+    render(boardContainer, new SortingView().getElement(), RenderPosition.BEFOREEND);
+    render(boardContainer, new TripEventsListView().getElement(), RenderPosition.BEFOREEND);
+    const tripEventsList = document.querySelector('.trip-events__list');
+    for (let i = 0; i < POINTS_COUNT; i++) {
+      renderPoint(tripEventsList, events[i], possibleOffers);
+    }
+  }
+};
+
 render(tripControlsNavigation, new MenuView().getElement(), RenderPosition.BEFOREEND);
 render(tripControlsFilters, new FiltersView().getElement(), RenderPosition.BEFOREEND);
-render(tripEvents, new SortingView().getElement(), RenderPosition.BEFOREEND);
-
-render(tripEvents, new TripEventsListView().getElement(), RenderPosition.BEFOREEND);
-
-const tripEventsList = document.querySelector('.trip-events__list');
-
-for (let i = 0; i < POINTS_COUNT; i++) {
-  renderPoint(tripEventsList, points[i], possibleOffers);
-}
+renderBoard(tripEvents, points);
